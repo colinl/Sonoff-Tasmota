218770eb9f8b8dc97b624fe8584d2cb16a015ee8
colinl <colin@clanlaw.org.uk>
2018-02-28 16:25:53 +0000

    Fixed messed up merge

--- sonoff/user_config.h
+++ sonoff/user_config.h
@@ -229,6 +229,4 @@
   #error "Select either USE_MQTT_TLS or USE_WEBSERVER as there is just not enough memory to play with"
 #endif
 
-#if (ARDUINO < 10610)
-  #error "This software is supported with Arduino IDE starting from 1.6.10 and ESP8266 Release 2.3.0"
-#endif
+#endif  // _USER_CONFIG_H_
