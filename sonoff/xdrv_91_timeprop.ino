e0ba64f5947d7074b6930c92c118b75b460594a4
colinl <colin@clanlaw.org.uk>
2018-03-03 09:49:57 +0000

    timeprop apparently working in basic form

--- sonoff/xdrv_91_timeprop.ino
+++ sonoff/xdrv_91_timeprop.ino
@@ -21,11 +21,14 @@
    the automation fails */
 
 #ifdef USE_TIMEPROP
+#define D_CMND_TIMEPROP "timeprop_"
+#define D_CMND_TIMEPROP_SETPOWER "setpower"    // data is power 0:1
+#define D_CMND_TIMEPROP_CMD_B "CmndB"
 
 enum TimepropCommands { CMND_TIMEPROP_SETPOWER, CMND_TIMEPROP_CMD_B };
 const char kTimepropCommands[] PROGMEM = D_CMND_TIMEPROP_SETPOWER "|" D_CMND_TIMEPROP_CMD_B;
 
-static double timeprop_power = 0.0;
+static float timeprop_power = 0.0;
 
 void Timeprop_Init()
 {
@@ -44,9 +47,65 @@ void Timeprop_Every_50ms() {
 }
 
 void Timeprop_Every_Second() {
-  ExecuteCommandPower(1, timeprop_power >= 0.5 ? 1 : 0);
-  //snprintf_P(log_data, sizeof(log_data), "Timeprop Every Second");
+  static int cycleTime = TIMEPROP_CYCLETIME;        // cycle time seconds, float to force float calcs
+  static int deadTime = TIMEPROP_DEADTIME;            // actuator action time seconds
+  static int invert = TIMEPROP_OPINVERT;              // whether to invert o/p
+
+  static float dtoc = (float)deadTime/cycleTime;
+  static int opState = -1;   // current output state, initiallise to illegal value to indicate unknown
+  int newState;
+  float wave;
+  float direction;
+  float effectivePower;
+
+  wave = (utc_time % cycleTime)/(float)cycleTime;
+  // determine direction of travel and convert to triangular wave
+  if (wave < 0.5) {
+    direction = 1;      // on the way up
+    wave = wave*2;
+  } else {
+    direction = -1;     // on the way down
+    wave = (1 - wave)*2;
+  }
+  //int wave_pc = wave * 100;
+  //snprintf_P(log_data, sizeof(log_data), "wave_pc: %d, direction %d", wave_pc, direction);
   //AddLog(LOG_LEVEL_INFO);
+  // if a dead_time has been supplied for this o/p then adjust power accordingly
+  if (deadTime > 0  && timeprop_power > 0.0  &&  timeprop_power < 1.0) {
+      effectivePower = (1.0-2.0*dtoc)*timeprop_power + dtoc;
+  } else {
+      effectivePower = timeprop_power;
+  }
+  //int epower_pc = effectivePower * 100;
+  //int power_pc = timeprop_power * 100;
+  //snprintf_P(log_data, sizeof(log_data), "power_pc: %d, epower_pc: %d", power_pc, epower_pc);
+  //AddLog(LOG_LEVEL_INFO);
+  //  cope with end cases in case values outside 0..1
+  if (effectivePower <= 0.0) {
+      newState = 0;     // no heat
+  } else if (effectivePower >= 1.0) {
+      newState = 1;     // full heat
+  } else {
+      // only allow power to come on on the way down and off on the way up, to reduce short pulses
+      if (effectivePower >= wave  &&  direction == -1) {
+          newState = 1;
+          //snprintf_P(log_data, sizeof(log_data), "setting to 1");
+          //AddLog(LOG_LEVEL_INFO);
+      } else if (effectivePower <= wave  &&  direction == 1) {
+          newState = 0;
+          //snprintf_P(log_data, sizeof(log_data), "setting to 0");
+          //AddLog(LOG_LEVEL_INFO);
+      } else {
+          // otherwise leave it as it is
+          newState = opState;
+          //snprintf_P(log_data, sizeof(log_data), "leaving as is");
+          //AddLog(LOG_LEVEL_INFO);
+      }
+  }
+  if (newState != opState) {
+    opState = newState;
+    ExecuteCommandPower(1, invert ? (1-opState) : opState);
+  }
 }
 
 /* struct XDRVMAILBOX { */
@@ -64,7 +123,7 @@ boolean Timeprop_Command()
   char command [CMDSZ];
   boolean serviced = true;
   uint8_t ua_prefix_len = strlen(D_CMND_TIMEPROP); // to detect prefix of command
-
+/*
   snprintf_P(log_data, sizeof(log_data), "Command called: "
     "index: %d data_len: %d payload: %d topic: %s data: %s\n",
     XdrvMailbox.index,
@@ -74,22 +133,20 @@ boolean Timeprop_Command()
     (XdrvMailbox.data_len >= 0 ? XdrvMailbox.data : ""));
 
     AddLog(LOG_LEVEL_INFO);
-
+*/
   if (0 == strncasecmp_P(XdrvMailbox.topic, PSTR(D_CMND_TIMEPROP), ua_prefix_len)) {
     // command starts with Timeprop
     int command_code = GetCommandCode(command, sizeof(command), XdrvMailbox.topic + ua_prefix_len, kTimepropCommands);
-    snprintf_P(log_data, sizeof(log_data), "Timeprop Command found: %d", command_code);
-
-      AddLog(LOG_LEVEL_INFO);
+    //snprintf_P(log_data, sizeof(log_data), "Timeprop Command found: %d", command_code);
+    //AddLog(LOG_LEVEL_INFO);
     if (CMND_TIMEPROP_SETPOWER == command_code) {
-      snprintf_P(log_data, sizeof(log_data), "Timeprop command timeprop_setpower called: "
-        "index: %d data_len: %d payload: %d topic: %s data: %s\n",
+      snprintf_P(log_data, sizeof(log_data), "Timeprop command timeprop_setpower: "
+        "index: %d data_len: %d payload: %d topic: %s data: %s",
 	      XdrvMailbox.index,
 	      XdrvMailbox.data_len,
 	      XdrvMailbox.payload,
 	      (XdrvMailbox.payload >= 0 ? XdrvMailbox.topic : ""),
 	      (XdrvMailbox.data_len >= 0 ? XdrvMailbox.data : ""));
-
         AddLog(LOG_LEVEL_INFO);
         timeprop_power = atof(XdrvMailbox.data);
     }
@@ -119,9 +176,8 @@ void Timeprop_Show_Sensor() {
 }
 
 void Timeprop_Set_Power() {
-  // todo: find out what this is used for
-  snprintf_P(log_data, sizeof(log_data), "Timeprop Set Power");
-  AddLog(LOG_LEVEL_INFO);
+  //snprintf_P(log_data, sizeof(log_data), "Timeprop Set Power");
+  //AddLog(LOG_LEVEL_INFO);
 }
 
 /*********************************************************************************************\
