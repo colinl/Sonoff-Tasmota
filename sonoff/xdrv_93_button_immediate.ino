1dcb6ff37f4addfa53f9640cddda5db29b1fdb7e
colinl <colin@clanlaw.org.uk>
2019-01-07 22:00:58 +0000

    button_immediate fixes from s008

--- sonoff/xdrv_93_button_immediate.ino
+++ sonoff/xdrv_93_button_immediate.ino
@@ -22,11 +22,11 @@
 
 // long press tick count 1 second @ 50ms
 #define BI_LONG_PRESS_TICK_COUNT 20
-// double press tick count, max 50 ms ticks since last pressed
-#define BI_DOUBLE_PRESS_TICK_COUNT 20
-boolean bi_pressed = false;
-int bi_tick_counter = 0;
-boolean  bi_long_press_passed = false;
+// double press tick count, max 50 ms ticks since last pressed (2 secs)
+#define BI_DOUBLE_PRESS_TICK_COUNT 40
+boolean bi_pressed[MAX_KEYS] = { false };
+int bi_tick_counter[MAX_KEYS] = { 0 };
+boolean  bi_long_press_passed[MAX_KEYS] = { false };
 
 
 /* struct XDRVMAILBOX { */
@@ -40,42 +40,53 @@ boolean  bi_long_press_passed = false;
 
 boolean ButtonImmediatePressed()
 {
-  if ((PRESSED == XdrvMailbox.payload) && (NOT_PRESSED == lastbutton[XdrvMailbox.index])) {
-
-    snprintf_P(log_data, sizeof(log_data), PSTR(D_LOG_APPLICATION D_BUTTON "%d " D_LEVEL_10), XdrvMailbox.index +1);
+  int index = XdrvMailbox.index;
+  if ((PRESSED == XdrvMailbox.payload) && (NOT_PRESSED == lastbutton[index])) {
+    snprintf_P(log_data, sizeof(log_data), PSTR(D_LOG_APPLICATION D_BUTTON "%d " D_LEVEL_10), index +1);
     AddLog(LOG_LEVEL_INFO);
     // the button has been pressed, check for quick double press
-    if (bi_tick_counter >= BI_DOUBLE_PRESS_TICK_COUNT) {
-      // not a double press,  toggle the relay
-      ExecuteCommandPower(XdrvMailbox.index + 1, POWER_TOGGLE, SRC_BUTTON);
+    if (bi_tick_counter[index] >= BI_DOUBLE_PRESS_TICK_COUNT)
+    {
+      // a long time since last press so not a double press,  toggle the relay
+      // beware, this changes XdrvMailbox I think
+      ExecuteCommandPower(index + 1, POWER_TOGGLE, SRC_BUTTON);
     }
     // restart the counter
-    bi_pressed = true;
-    bi_tick_counter = 0;
-    bi_long_press_passed = false;
+    bi_pressed[index] = true;
+    bi_tick_counter[index] = 0;
+    bi_long_press_passed[index] = false;
     // and send MQTT toggle provided ButtonTopic is set
-    SendKey(0, XdrvMailbox.index + 1, POWER_TOGGLE);
-  } else if ((NOT_PRESSED == XdrvMailbox.payload) && (PRESSED == lastbutton[XdrvMailbox.index])) {
+    SendKey(0, index + 1, POWER_TOGGLE);
+  } else if ((NOT_PRESSED == XdrvMailbox.payload) && (PRESSED == lastbutton[index]))
+  {
 
-    snprintf_P(log_data, sizeof(log_data), PSTR(D_LOG_APPLICATION D_BUTTON "%d " D_LEVEL_01 "  %d ticks"), XdrvMailbox.index +1, bi_tick_counter);
+    snprintf_P(log_data, sizeof(log_data), PSTR(D_LOG_APPLICATION D_BUTTON "%d " D_LEVEL_01 "  %d ticks"), index + 1, bi_tick_counter[index]);
     AddLog(LOG_LEVEL_INFO);
     // the button has been released
-    bi_pressed = false;
+    bi_pressed[index] = false;
   }
   return true;  // Serviced here
 }
 
-void ButtonImmediateTick() {
+void ButtonImmediateTick()
+{
   // called every 50ms
-  bi_tick_counter++;
-  // is the button pressed?
-  if (bi_pressed  &&  !bi_long_press_passed) {
-    // yes increment the counter and see if enough time has passed
-    if (bi_tick_counter >= BI_LONG_PRESS_TICK_COUNT) {
-      // stop this happening again
-      bi_long_press_passed = true;
-      // send a long press mqtt message
-      SendKey(0, XdrvMailbox.index + 1, 3);
+  uint8_t i;
+  uint8_t max = (devices_present > MAX_KEYS) ? MAX_KEYS : devices_present;
+  for ( i = 0; i < max; i++ )
+  {
+    bi_tick_counter[i]++;
+    // is the button pressed?
+    if (bi_pressed[i]  &&  !bi_long_press_passed[i])
+    {
+      // yes increment the counter and see if enough time has passed
+      if (bi_tick_counter[i] >= BI_LONG_PRESS_TICK_COUNT)
+      {
+        // stop this happening again
+        bi_long_press_passed[i] = true;
+        // send a long press mqtt message
+        SendKey(0, i + 1, 3);
+      }
     }
   }
 }
